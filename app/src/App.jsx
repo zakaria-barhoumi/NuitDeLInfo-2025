b534689973dce368686a1ccd901fc8b255951b11
zouaghi-rayen <zouaghi.rayen2003@gmail.com>
2025-12-04 17:46:00 +0100

    Initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <p className="name">Rayen</p>
+        <div className="test-word-container">
+          <p className="test-word">test</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
