6142bb0f9c1c3d1c676fa747ddc1b976920d3dac
zouaghi-rayen <zouaghi.rayen2003@gmail.com>
2025-12-04 18:29:29 +0100

    Added routing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import Quiz from './Quiz';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <p className="name">Rayen</p>
-        <div className="test-word-container">
-          <p className="test-word">test</p>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </Router>
   );
 }
 
